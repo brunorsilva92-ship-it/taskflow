ce78d00c0d22917c34caa67b51732051658dbabe
brunorsilva92-ship-it <bruno.r.silva92@aluno.senai.br>
2026-08-25 11:22:52 -0300

    25/08

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -1,8 +1,10 @@
+import "./sobre.css";
+
 function Sobre() {
   return (
     <div>
-      <h1>Sobre</h1>
-      <p>
+      <h1 className="titulo">Sobre</h1>
+      <p className="subtitulo">
         O TaskFlow é uma solução simples e eficiente para organização e acompanhamento de tarefas do dia a dia. Inspirado na metodologia Kanban, o sistema organiza as suas atividades em um fluxo visual dividido em três etapas claras: A Fazer, Em Andamento e Concluído. Desenvolvido com tecnologia moderna em React, o aplicativo oferece uma experiência fluida para criar, editar, mover e excluir tarefas em tempo real. Todas as suas informações são salvas de forma segura na nuvem por meio da integração com uma API dedicada, garantindo praticidade, responsividade e alto desempenho em qualquer dispositivo.
       </p>
     </div>
